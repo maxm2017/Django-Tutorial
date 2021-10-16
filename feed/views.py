01924e1498ba55f557487acf56252bc96c4f766a
Max Mercel <mmercel8@gmail.com>
2021-10-16 18:31:23 +0100

    Added details page containing full size image, clicking an image links to full size image

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 # Create your views here.
 
 from .models import Post
@@ -10,3 +10,7 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.all()
         return context
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
